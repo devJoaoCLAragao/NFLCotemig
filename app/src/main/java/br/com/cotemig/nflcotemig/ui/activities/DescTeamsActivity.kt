1c7e9bb46487100ec14284fc4b67b564f7e4ab80
Pedro Pereira <78936854+PedroMeiraPereira@users.noreply.github.com>
2021-06-06 22:49:03 -0300

    Adicionados Twitter e Instagram
    Implementação concluída dos botões do Twitter e Instagram, tanto no app, quanto no browser

--- app/src/main/java/br/com/cotemig/nflcotemig/ui/activities/DescTeamsActivity.kt
+++ app/src/main/java/br/com/cotemig/nflcotemig/ui/activities/DescTeamsActivity.kt
@@ -5,17 +5,12 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import br.com.cotemig.nflcotemig.R
-import br.com.cotemig.nflcotemig.model.ListaTeams
 import br.com.cotemig.nflcotemig.model.Teams
-import br.com.cotemig.nflcotemig.service.RetrofitInitializer
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_descteams.*
-import retrofit2.Call
-import retrofit2.Response
 
 class DescTeamsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +32,14 @@ class DescTeamsActivity : AppCompatActivity() {
             val facebookUrl = getFacebookPageURL(this, team.strFacebook)
             facebookIntent.data = Uri.parse(facebookUrl)
             startActivity(facebookIntent)
+        }
+
+        strTwitter.setOnClickListener {
+            openTwitter(team.strTwitter)
+        }
+
+        strInstagram.setOnClickListener {
+            openInstagram(team.strInstagram)
         }
     }
 
@@ -77,6 +80,30 @@ class DescTeamsActivity : AppCompatActivity() {
             }
         } catch (e: PackageManager.NameNotFoundException) {
             FACEBOOK_URL //normal web url
+        }
+    }
+
+    fun openTwitter(url: String){
+        try {
+            // Check if the Twitter app is installed on the phone.
+            packageManager.getPackageInfo("com.twitter.android", 0)
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://$url")))
+        } catch (e: PackageManager.NameNotFoundException) {
+            // If Twitter app is not installed, start browser.
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://$url")))
+        }
+    }
+
+    fun openInstagram(url: String){
+        val uri = Uri.parse("http://$url")
+        val likeIng = Intent(Intent.ACTION_VIEW, uri)
+
+        likeIng.setPackage("com.instagram.android")
+
+        try {
+            startActivity(likeIng)
+        } catch (e: ActivityNotFoundException) {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://$url")))
         }
     }
 
